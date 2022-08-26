d7739aafa0d2017dde89dde9cccf583e83fe9a59
Doniyorbek <doniyorbekxakimbekov7007@gmail.com>
2022-08-26 15:37:11 +0300

    INFRA: upload project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Siz va Kescha yoshlari farqi:\n");
+
+System.Console.Write("Kescha ni yoshini kiriting: ");
+int keschaAge = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Sizni yoshingiz: ");
+int personAge = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write($"O'rtadagi faqar: {Math.Abs(keschaAge - personAge)} yosh :)");
+
